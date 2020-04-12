5758ee12a3f9e4504c5ef97123912b99e55462e0
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-12 21:34:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,4 +19,4 @@ end
 # Listening on 127.0.0.1:9393, CTRL+C to stop
 # /Users/avi/.rvm/gems/ruby-2.2.2/gems/eventmachine-1.0.8/lib/eventmachine.rb:534:in `start_tcp_server': no acceptor (port is in use or requires root privileges) (RuntimeError)
 
-This means you have another Shotgun server running somewhere. Do you have another terminal or shell open
+# This means you have another Shotgun server running somewhere. Do you have another terminal or shell open unning a web application or Shotgun? You need to find that process or tab that is running a web application using Shotgun and shut it down with CTRL+C.
